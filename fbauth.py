2a44c445e6bc4af587368219d70e469b1dc71c95
bigstar9906 <bigstar9906@naver.com>
2022-12-07 21:01:05 +0900

    [FEAT] Write Authentication APIs

--- fbauth.py
+++ fbauth.py
@@ -1,9 +1,43 @@
+from flask import request
 from flask_restx import Resource, Namespace
+from firebase_admin import auth
 
+from fbInitallize import pb
 
 FbAuth = Namespace('FbAuth')
 
-@FbAuth.route('/test')
-class AuthTest(Resource):
-    def get(self):
-        return "OK",200
+
+@FbAuth.route("/signup")
+class SignUp(Resource):
+    def post(self):
+        email = request.form.get('email')
+        password = request.form.get('password')
+        if email =="" or password =="" :
+            error=[]
+            if email =="" :
+                error.append("Username is Missing ")                
+            if password =="":
+                error.append("Password is Missing ")
+            return error, 400
+        else:
+            try :
+                user = auth.create_user(
+                    email = email,
+                    password = password
+                )
+                return {'message':f'Successfully create user{user.uid}'}, 200
+            except:
+                return "Error occur in creating user",400
+
+@FbAuth.route('/signin')
+class SignIn(Resource):
+    def post(self):
+        email = request.form.get('email')
+        password = request.form.get('password')
+        try:
+            user = pb.auth().sign_in_with_email_and_password(email,password)
+            jwt = user['idToken']
+            return {'token':jwt},200
+        except:
+            return {'message':'There was an error logging in'},400  
+
