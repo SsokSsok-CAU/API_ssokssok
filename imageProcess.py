95bae39fae8fee9ea784aa0b7ea8dd83e5b19deb
bigstar9906 <bigstar9906@naver.com>
2022-12-09 04:10:41 +0900

    [FEAT] Get file for filename / Get files for group

--- imageProcess.py
+++ imageProcess.py
@@ -42,6 +42,7 @@ class ImageProcessing(Resource):
         pbstorage.child(user_email+'/convertImage/'+filename).put("convertImg"+filename)
         png_result_url = pbstorage.child(user_email+'/convertImage/'+filename).get_url(tok)
         svg_fliename = PngToSvg("convertImg"+filename)
+        svg_fliename = svg_fliename[10:len(svg_fliename)]
         pbstorage.child(user_email+'/Coloring/'+svg_fliename).put(svg_fliename)
         svg_result_url = pbstorage.child(user_email+'/Coloring/'+svg_fliename).get_url(tok)
         FileManage()
@@ -101,6 +102,23 @@ class ImageProcessing(Resource):
         if len(my_files_url)==0:
             return {'ErrorMessage':"There is no File before Convert for "+user_email},400
         return my_files_url,200
+    
+@ImageProcess.route('/getfile/beforeConvert')
+class ImageProcessing(Resource):
+    def get(self):
+        tok = request.headers['Authorization']
+        user_email = ParseJwtPayLoad(tok)['email']
+        request_filename =  request.args.get('filename')
+        my_files_url=[]
+        all_files = pbstorage.list_files()
+        for file in all_files:
+            fn = file.name.split('/')
+            if len(fn)>2:
+                if  fn[0]==user_email and fn[1]=='Image'and fn[2]==request_filename:
+                    my_files_url.append(pbstorage.child(file.name).get_url(tok))
+        if len(my_files_url)==0:
+            return {'ErrorMessage':"There is no File "+request_filename+" before Convert for "+user_email},400
+        return my_files_url[0],200
 
 @ImageProcess.route('/myfiles/afterConvert')
 class ImageProcessing(Resource):
@@ -117,6 +135,23 @@ class ImageProcessing(Resource):
         if len(my_files_url)==0:
             return {'ErrorMessage':"There is no Converted Files for "+user_email},400
         return my_files_url,200
+
+@ImageProcess.route('/getfile/afterConvert')
+class ImageProcessing(Resource):
+    def get(self):
+        tok = request.headers['Authorization']
+        user_email = ParseJwtPayLoad(tok)['email']
+        request_filename =  request.args.get('filename')
+        my_files_url=[]
+        all_files = pbstorage.list_files()
+        for file in all_files:
+            fn = file.name.split('/')
+            if len(fn)>2:
+                if  fn[0]==user_email and fn[1]=='convertImage'and fn[2]==request_filename:
+                    my_files_url.append(pbstorage.child(file.name).get_url(tok))
+        if len(my_files_url)==0:
+            return {'ErrorMessage':"There is no File "+request_filename+" after Convert for "+user_email},400
+        return my_files_url[0],200
     
     
 @ImageProcess.route('/myfiles/Coloring')
@@ -134,4 +169,97 @@ class ImageProcessing(Resource):
         if len(my_files_url)==0:
             return {'ErrorMessage':"There is no Coloring Files for "+user_email},400
         return my_files_url,200
-        
+
+@ImageProcess.route('/getfile/Coloring')
+class ImageProcessing(Resource):
+    def get(self):
+        tok = request.headers['Authorization']
+        user_email = ParseJwtPayLoad(tok)['email']
+        request_filename =  request.args.get('filename')
+        my_files_url=[]
+        all_files = pbstorage.list_files()
+        for file in all_files:
+            fn = file.name.split('/')
+            if len(fn)>2:
+                if  fn[0]==user_email and fn[1]=='Coloring'and fn[2]==request_filename:
+                    my_files_url.append(pbstorage.child(file.name).get_url(tok))
+        if len(my_files_url)==0:
+            return {'ErrorMessage':"There is no Coloring File "+request_filename+" for "+user_email},400
+        return my_files_url[0],200
+
+@ImageProcess.route('/myfiles/groupby')
+class ImageProcessing(Resource):
+    def get(self):
+        tok = request.headers['Authorization']
+        user_email = ParseJwtPayLoad(tok)['email']
+        my_files_url=[]
+        real_filename = ""
+        all_files = sorted(pbstorage.list_files(),key=lambda x:x.updated,reverse=True)
+        for file in all_files:
+            filename = file.name.split('/')
+            if len(filename)>2:
+                if filename[0]==user_email and filename[1]=='Coloring' and not filename[2]=="":
+                    filename = filename.split('.')[0]
+                    real_filename = filename[10:len(filename)]
+                    file_info = {"filename":real_filename,"svg":pbstorage.child(file.name).get_url(tok)}
+                    my_files_url.append(file_info)
+        for info in my_files_url:
+            for file in all_files:
+                fn = file.name.split('/')
+                if len(fn)>2:
+                    if fn[0]==user_email and fn[2].split('.')[0]==info.filename:
+                        if fn[1]=='Image':
+                            info['origin'] = pbstorage.child(file.name).get_url(tok)
+                        elif fn[1]=='convertImg':
+                            info['convert'] = pbstorage.child(file.name).get_url(tok)
+        if len(my_files_url)==0:
+            return {'ErrorMessage':"There is no Coloring Files for "+user_email},400
+        return my_files_url,200
+    
+@ImageProcess.route('/getfile/Submit')
+class ImageProcessing(Resource):
+    def get(self):
+        tok = request.headers['Authorization']
+        user_email = ParseJwtPayLoad(tok)['email']
+        request_filename =  request.args.get('filename')
+        my_files_url=[]
+        all_files = pbstorage.list_files()
+        for file in all_files:
+            fn = file.name.split('/')
+            if len(fn)>2:
+                if  fn[0]==user_email and fn[1]=='Submit'and fn[2]==request_filename:
+                    my_files_url.append(pbstorage.child(file.name).get_url(tok))
+        if len(my_files_url)==0:
+            return {'ErrorMessage':"There is no Submitted File "+request_filename+" for "+user_email},400
+        return my_files_url[0],200
+    
+@ImageProcess.route('/myfiles/groupby')
+class ImageProcessing(Resource):
+    def get(self):
+        tok = request.headers['Authorization']
+        user_email = ParseJwtPayLoad(tok)['email']
+        my_files_url=[]
+        real_filename = ""
+        all_files = sorted(pbstorage.list_files(),key=lambda x:x.updated,reverse=True)
+        for file in all_files:
+            filename = file.name.split('/')
+            if len(filename)>2:
+                if filename[0]==user_email and filename[1]=='Coloring' and not filename[2]=="":
+                    fn = filename[2].split('.')[0]
+                    real_filename = fn[10:len(fn)]
+                    file_info = {"filename":real_filename,"svg":pbstorage.child(file.name).get_url(tok)}
+                    my_files_url.append(file_info)
+        for info in my_files_url:
+            for file in all_files:
+                fn = file.name.split('/')
+                if len(fn)>2:
+                    if fn[0]==user_email and fn[2].split('.')[0]==info['filename']:
+                        if fn[1]=='Image':
+                            info['origin'] = pbstorage.child(file.name).get_url(tok)
+                        elif fn[1]=='convertImage':
+                            info['convert'] = pbstorage.child(file.name).get_url(tok)
+        if len(my_files_url)==0:
+            return {'ErrorMessage':"There is no Coloring Files for "+user_email},400
+        return my_files_url,200
+
+      
